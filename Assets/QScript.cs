08bfd5e126d94936528ce0c3a3b64a152c20ece5
QuinnWuest <qwuest123@gmail.com>
2023-12-12 01:02:27 -0500

    Q

---
+++ Assets/QScript.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+using KModkit;
+using UnityEngine.Video;
+
+public class QScript : MonoBehaviour
+{
+    public KMBombModule Module;
+    public KMBombInfo BombInfo;
+    public KMAudio Audio;
+
+    public KMSelectable QSel;
+    public GameObject PictureObj;
+    public Texture[] PictureTextures;
+
+    private int _moduleId;
+    private static int _moduleIdCounter = 1;
+    private bool _moduleSolved;
+
+    private bool _isPopped;
+    private bool _isSelected;
+    private string _input;
+    private int _chosenPicture;
+    private string _solution;
+    private static bool _multiLetter;
+
+    private void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+        var ModSelectable = GetComponent<KMSelectable>();
+        ModSelectable.OnFocus += delegate () { _isSelected = true; };
+        ModSelectable.OnDefocus += delegate () { _isSelected = false; };
+        QSel.OnInteract += QPress;
+        StartCoroutine(Rainbow());
+        Debug.LogFormat("[Q #{0}] Q.", _moduleId);
+    }
+
+    private bool QPress()
+    {
+        if (_moduleSolved)
+            return false;
+        _isPopped = true;
+        Audio.PlaySoundAtTransform("Pop", transform);
+        QSel.AddInteractionPunch(1f);
+        QSel.gameObject.transform.localScale = new Vector3(0f, 0f, 0f);
+        newSolution:
+        _chosenPicture = Rnd.Range(0, PictureTextures.Length);
+        var name = PictureTextures[_chosenPicture].name;
+        var ix = name.IndexOf(' ');
+        if (ix == -1)
+            _solution = name;
+        else
+            _solution = name.Substring(0, ix);
+        if (_solution.Length > 1 && !_multiLetter)
+            goto newSolution;
+        PictureObj.GetComponent<MeshRenderer>().material.mainTexture = PictureTextures[_chosenPicture];
+        PictureObj.SetActive(true);
+        return false;
+    }
+
+    private IEnumerator Rainbow()
+    {
+        while (true)
+        {
+            var duration = Rnd.Range(6.9f, 13.37f);
+            var elapsed = 0f;
+            while (elapsed < duration)
+            {
+                QSel.gameObject.GetComponent<MeshRenderer>().material.color = Color.HSVToRGB(Mathf.Lerp(0f, 1f, elapsed / duration), 0.69f, 1f);
+                yield return null;
+                elapsed += Time.deltaTime;
+            }
+            yield return null;
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (!_isSelected || !_isPopped || _moduleSolved)
+            return;
+        Event e = Event.current;
+        if (e.type != EventType.KeyDown)
+            return;
+        ProcessKey(e.keyCode);
+    }
+
+    private void OnDestroy()
+    {
+        _multiLetter = true;
+    }
+
+    private void ProcessKey(KeyCode key)
+    {
+        QSel.AddInteractionPunch(0.2f);
+        if (key < KeyCode.A || key > KeyCode.Z)
+            return;
+        string add = key.ToString().ToUpperInvariant();
+        _input += add;
+        if (_input.Contains(_solution))
+        {
+            Debug.LogFormat("[Q #{0}] {1}!!!", _moduleId, PictureTextures[_chosenPicture].name);
+            Audio.PlaySoundAtTransform("beagle", transform);
+            _moduleSolved = true;
+            Module.HandlePass();
+        }
+    }
+}
